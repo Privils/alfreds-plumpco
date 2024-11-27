95f31b83730c5b729e4b75db3402e17443f69426
Priviledge <privilledgeblack8@gmail.com>
2024-11-27 23:23:48 +0200

    serives and about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { FaWhatsapp, FaArrowRight } from "react-icons/fa";
+import { FaWhatsapp, FaArrowRight, FaPhone } from "react-icons/fa";
 import image from "./images/service-1.jpg";
 import image1 from "./images/service-2.jpg";
 import image2 from "./images/service-3.jpg";
-import image3 from './images/about-1.jpg'
+import image3 from './images/about-1.jpg';
+import image4 from './images/about-1.jpg';
+import image5 from './images/about-2.jpg';
 
 const Home = () => {
   return (
@@ -58,7 +60,7 @@ const Home = () => {
       {/* fix this section */}
       <section className="about">
         <h4>about us</h4>
-        <div>
+        <div className="a-bout">
           <div className="about-text">
             <h1>We Are A Trusted Plumbing Company</h1>
             <p>
@@ -80,12 +82,88 @@ const Home = () => {
                 Immediate 24/ 7 emergency services
                 </li>
               </ul>
-              <div>
-                //phoone
-              </div>
+              <div className='phone-container inAbout'>
+       <FaPhone className='FaPhone'/>
+       <p className='call-us-about'>
+       Got a Plumbing Emergency? Call us 24/7: <br />
+       <span className="phone">+27 84 051 5349</span>
+       </p>
+        </div>
           </div>
           <div className="about-us-img-container">
-            <img src="" alt="" />
+            <img src={image4} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="services">
+
+        <div className="years">
+          <p>
+            15 years experience
+          </p>
+        </div>
+
+        <div className="services-container">
+          <h3>our services</h3>
+          <p className="Explore-Our-Services">Explore Our Services</p>
+          <p className="offer-text">
+          We offer a wide range of plumbing services to meet your needs, all with no <span className="callout-fee">callout fee</span>
+          </p>
+          <div className="cards-container">
+           <div className="service-cards">
+            <span>icon</span>
+            <h2>
+            Drain Repair
+            </h2>
+            <p>
+            Whether it's a clogged drain, slow drainage, or a more complex problem, we provide effective solutions tailored to your needs.
+            </p>
+           </div>
+           <div className="service-cards">
+           <span>icon</span>
+            <h2>
+            Toilet Pipe Repair
+            </h2>
+            <p>
+            From fixing leaks to addressing pipe blockages, our team ensures that your toilet plumbing is functioning optimally.
+            </p>
+           </div>
+           <div className="service-cards">
+           <span>icon</span>
+            <h2>
+            Sewer Line Repair
+            </h2>
+            <p>
+            Our team is adept at diagnosing and resolving sewer line issues, ensuring the integrity of your plumbing system.
+            </p>
+           </div>
+           <div className="service-cards">
+           <span>icon</span>
+            <h2>
+            Water Heater Repair
+            </h2>
+            <p>
+            Whether you're experiencing issues with temperature control or a complete breakdown, our skilled technicians deliver efficient solutions.
+            </p>
+           </div>
+           <div className="service-cards">
+           <span>icon</span>
+            <h2>
+            Burst Geyser Replacement
+            </h2>
+            <p>
+            Our skilled technicians are equipped to handle urgent geyser replacements, ensuring your hot water supply is restored efficiently.
+            </p>
+           </div>
+           <div className="service-cards">
+           <span>icon</span>
+            <h2>
+            Emergency Plumbing Services
+            </h2>
+            <p>
+            Whether it's a burst pipe, sudden leaks, or any plumbing emergency, we are here to provide immediate assistance with quality service and customer satisfaction guaranteed.
+            </p>
+           </div>
           </div>
         </div>
       </section>
